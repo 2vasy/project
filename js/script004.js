73d423aec887ee645918938ea69bbeecdd72d869
wassili <2vasy@mail.ru>
2022-05-06 20:53:57 +0200

    add 004

--- js/script004.js
+++ js/script004.js
@@ -11,9 +11,9 @@
 
 const answers = [];
 
-//answers[0] = prompt("Как ваше имя?",'');
-//answers[1] = prompt("Как ваше фамилия?",'');
-//answers[2] = prompt("Сколько вам лет?",'');
+// answers[0] = prompt("Как ваше имя?",'');
+// answers[1] = prompt("Как ваше фамилия?",'');
+// answers[2] = prompt("Сколько вам лет?",'');
 
 //document.write(answers);
 console.log(typeof(answers));
