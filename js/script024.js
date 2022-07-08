4bccf73b9adeb05c8b6457590055697d7ef41310
Wassili <vasiliykulesh111@gmail.com>
2022-07-08 14:17:45 +0200

    script024 aus linux

---
+++ js/script024.js
@@ -0,0 +1,43 @@
+"use strict";
+//----1)
+
+//let x = 5; alert( x++ ); kommt 5 
+
+//let x = 5; alert( ++x );  kommt 6
+
+//----2)
+
+// [] + false - null + true
+
+// console.log(typeof([] + false)); // "false"
+
+//  console.log([] + false - null + true); // NaN
+
+//----3)
+
+//let y = 1;
+//let x = y = 2;
+//alert(x);             // 2
+
+// ----4)
+
+//console.log([] + 1 + 2); // "12"
+
+// ----5)
+
+//  alert("1"[0]);    // 1
+
+
+// ----6)
+
+// console.log(2 && 1 && null && 0 && undefined); .......
+
+// ----7)
+
+//console.log(!!( 1 && 2 ) === ( 1 && 2)); // false
+
+
+// ----7)
+                  3
+alert( null || 2 && 3 || 4 );
+
