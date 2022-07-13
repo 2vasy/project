6c95cad05fe87afe9d5ca70269736813a9d42c11
wassili <vasiliykulesh111@gmail.com>
2022-07-13 22:23:31 +0200

    script026 17:56

--- js/script025.js
+++ js/script025.js
@@ -4,18 +4,18 @@ const box = document.getElementById('box'); // id = box
 
 console.log(box); //work only in the browser
 
-const btns = dokument.getElementsByTagName('button'); // ('button)[1];
+const btns = document.getElementsByTagName('button'); // ('button)[1];
 
 console.log(btns); //(btns[1]);// HTML Collection 
 
-const circles = dokument.getElementsByClassName('circle');
+const circles = document.getElementsByClassName('circle');
 
 console.log(circles);
 
 
 
 
-const hearts = dokument.querySelectorAll('.heart'); // forEach  // querySelector -- All elements
+const hearts = document.querySelectorAll('.heart'); // forEach  // querySelector -- All elements
 
 
 console.log(hearts);
--- js/script026.js
+++ js/script026.js
@@ -1,6 +1,35 @@
 'use strict';
 
-const box = document.getElementById('box');
-const btns = dokument.getElementsByTagName('button');
-const circles = dokument.getElementsByClassName('circle');
-const hearts = dokument.querySelectorAll('.heart');
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+//box.style.cssText = `background-color: blue; width: ${num}px`;
+
+box.style.cssText = 'background-color: blue; width: 500px';
+
+btns[1].style.borderRadius = '100%';
+// circles.style.backgroundColor = 'red'; fehler
+circles[0].style.backgroundColor = 'red'; // richtig
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+//const text = document.createTextNode('ich bin da');
+
+div.classList.add('black');
+
+//document.body.append(div); // die Methode (append) bedeutet dass div  am ende commt
+//document.querySelector('.wrapper').append(div);
+//wrapper.append(div);
