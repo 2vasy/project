aa0f5201150d4c1856ba058280a958a5392e9204
Wassili <vasiliykulesh111@gmail.com>
2022-07-13 14:40:26 +0200

    script026

---
+++ js/script026.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const box = document.getElementById('box');
+const btns = dokument.getElementsByTagName('button');
+const circles = dokument.getElementsByClassName('circle');
+const hearts = dokument.querySelectorAll('.heart');
